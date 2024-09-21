ddaca85a40b96b8b3db0f924fe53628fa181e1da
TheRedBrain <thetrueredbrain@gmail.com>
2024-09-21 12:36:38 +0200

    added test mod

---
+++ src/testmod/java/com/github/theredbrain/rpgcrafting_test/RPGCraftingTest.java
@@ -0,0 +1,15 @@
+package com.github.theredbrain.rpgcrafting_test;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RPGCraftingTest implements ModInitializer {
+	public static final String MOD_ID = "rpgcrafting_test";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Testing RPG Crafting");
+	}
+}
---
+++ src/testmod_client/java/com/github/theredbrain/rpgcrafting_test/RPGCraftingTestClient.java
@@ -0,0 +1,10 @@
+package com.github.theredbrain.rpgcrafting_test;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RPGCraftingTestClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+	}
+}
