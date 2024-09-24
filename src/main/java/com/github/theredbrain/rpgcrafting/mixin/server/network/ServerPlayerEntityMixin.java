2a155b0ade312246f71db9e922c6e4323400d944
TheRedBrain <thetrueredbrain@gmail.com>
2024-09-24 18:55:45 +0200

    stash inventory is copied when player respawns

---
+++ src/main/java/com/github/theredbrain/rpgcrafting/mixin/server/network/ServerPlayerEntityMixin.java
@@ -0,0 +1,17 @@
+package com.github.theredbrain.rpgcrafting.mixin.server.network;
+
+import com.github.theredbrain.rpgcrafting.entity.player.DuckPlayerEntityMixin;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerPlayerEntity.class)
+public class ServerPlayerEntityMixin {
+
+    @Inject(method = "copyFrom", at = @At("TAIL"))
+    public void rpgcrafting$copyFrom(ServerPlayerEntity oldPlayer, boolean alive, CallbackInfo ci) {
+        ((DuckPlayerEntityMixin) this).rpgcrafting$setStashInventory(((DuckPlayerEntityMixin) oldPlayer).rpgcrafting$getStashInventory());
+    }
+}
